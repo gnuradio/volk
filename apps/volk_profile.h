16463a10a22626993bda67c44021ab0fba1b9a61
Nathan West <nathan.west@okstate.edu>
2015-03-30 17:21:35 -0500

    profile: make update actually update (append) to volk_config

--- apps/volk_profile.h
+++ apps/volk_profile.h
@@ -1,5 +1,5 @@
 
 
 void read_results(std::vector<volk_test_results_t> *results);
-void write_results(const std::vector<volk_test_results_t> *results);
+void write_results(const std::vector<volk_test_results_t> *results, bool update_result);
 void write_json(std::ofstream &json_file, std::vector<volk_test_results_t> results);
