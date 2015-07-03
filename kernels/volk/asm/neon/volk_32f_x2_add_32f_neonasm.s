d5903af97cf6c801821be5e962f58e1acd179b8e
Nathan West <nathan.west@okstate.edu>
2015-07-02 20:19:14 -0400

    kernels: use existing add neon asm kernels

--- kernels/volk/asm/neon/volk_32f_x2_add_32f_neonasm.s
+++ kernels/volk/asm/neon/volk_32f_x2_add_32f_neonasm.s
@@ -1,6 +1,6 @@
-@ static inline void volk_32f_x2_add_32f_a_neonpipeline(float* cVector, const float* aVector, const float* bVector, unsigned int num_points);
-	.global	volk_32f_x2_add_32f_a_neonpipeline
-volk_32f_x2_add_32f_a_neonpipeline:
+@ static inline void volk_32f_x2_add_32f_neonasm(float* cVector, const float* aVector, const float* bVector, unsigned int num_points);
+	.global	volk_32f_x2_add_32f_neonasm
+volk_32f_x2_add_32f_neonasm:
 	@ r0 - cVector: pointer to output array
 	@ r1 - aVector: pointer to input array 1
 	@ r2 - bVector: pointer to input array 2
@@ -15,35 +15,26 @@ volk_32f_x2_add_32f_a_neonpipeline:
 	bVal .req q1 @ d2-d3
 	cVal .req q2 @ d4-d5
 
+	@ AAPCS Section 5.1.1
+	@ A subroutine must preserve the contents of the registers r4-r8, r10, r11 and SP
 	stmfd	sp!, {r7, r8, sl}	@ prologue - save register states
-
-	pld [aVector, #128] @ pre-load hint - this is implementation specific!
-	pld [bVector, #128] @ pre-load hint - this is implementation specific!
 
 	movs quarterPoints, num_points, lsr #2
 	beq .loop2 @ if zero into quarterPoints
 
-	mov number, quarterPoints
-
-	@ Optimizing for pipeline
-	vld1.32	{d0-d1}, [aVector:128]!	@ aVal
-	vld1.32	{d2-d3}, [bVector:128]!	@ bVal
-	subs number, number, #1
-
+	mov	number, #0	@ number, 0
 .loop1:
 	pld [aVector, #128] @ pre-load hint - this is implementation specific!
 	pld [bVector, #128] @ pre-load hint - this is implementation specific!
-	vadd.f32 cVal, bVal, aVal
-	vld1.32 {d0-d1}, [aVector:128]! @ aVal
-	vld1.32 {d2-d3}, [bVector:128]! @ bVal
-	vst1.32	{d4-d5}, [cVector:128]! @ cVal
 
-	subs number, number, #1
-	bne	.loop1	@ first loop
-
-	@ One more time
+	vld1.32	{d0-d1}, [aVector:128]!	@ aVal
+	add	number, number, #1
+	vld1.32	{d2-d3}, [bVector:128]!	@ bVal
 	vadd.f32 cVal, bVal, aVal
-	vst1.32	{d4-d5}, [cVector:128]! @ cVal
+	cmp	number, quarterPoints
+	vst1.32	{d4-d5}, [cVector:128]!	@ cVal
+
+	ble	.loop1	@ first loop
 
 	mov	number, quarterPoints, asl #2
 
@@ -61,8 +52,3 @@ volk_32f_x2_add_32f_a_neonpipeline:
 .done:
 	ldmfd	sp!, {r7, r8, sl} @ epilogue - restore register states
 	bx	lr
-
-
-
-
-
