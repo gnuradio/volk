f6ce85eff55132dac453f6d8ab59c5a9bb0c4994
Marcus Müller <marcus@hostalia.de>
2016-05-14 14:24:38 +0200

    added complex/complex division kernel (generic & SSE3 only)

---
+++ kernels/volk/volk_32fc_x2_divide_32fc.h
@@ -0,0 +1,240 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2012, 2014, 2016 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * GNU Radio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * GNU Radio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Radio; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+/*!
+ * \page volk_32fc_x2_divide_32fc
+ *
+ * \b Overview
+ *
+ * Divide first vector of complexes element-wise by second.
+ *
+ * <b>Dispatcher Prototype</b>
+ * \code
+ * void volk_32fc_x2_divide_32fc(lv_32fc_t* cVector, const lv_32fc_t* numeratorVector, const lv_32fc_t* denumeratorVector, unsigned int num_points);
+ * \endcode
+ *
+ * \b Inputs
+ * \li numeratorVector: The numerator complex values.
+ * \li numeratorVector: The denumerator complex values.
+ * \li num_points: The number of data points.
+ *
+ * \b Outputs
+ * \li outputVector: The output vector complex floats.
+ *
+ * \b Example
+ * divide a complex vector by itself, demonstrating the result should be pretty close to 1+0j.
+ *
+ * \code
+ *   int N = 10;
+ *   unsigned int alignment = volk_get_alignment();
+ *   lv_32fc_t* input_vector  = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t)*N, alignment);
+ *   lv_32fc_t* out = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t)*N, alignment);
+ *
+ *   float delta = 2.f*M_PI / (float)N;
+ *   for(unsigned int ii = 0; ii < N; ++ii){
+ *       float real_1 = std::cos(0.3f * (float)ii);
+ *       float imag_1 = std::sin(0.3f * (float)ii);
+ *       input_vector[ii] = lv_cmake(real_1, imag_1);
+ *   }
+ *
+ *   volk_32fc_x2_divide_32fc(out, input_vector, input_vector, N);
+ *
+ *   for(unsigned int ii = 0; ii < N; ++ii){
+ *       printf("%1.4f%+1.4fj,", lv_creal(out[ii]), lv_cimag(out[ii]));
+ *   }
+ *   printf("\n");
+ *
+ *   volk_free(input_vector);
+ *   volk_free(out);
+ * \endcode
+ */
+
+#ifndef INCLUDED_volk_32fc_x2_divide_32fc_u_H
+#define INCLUDED_volk_32fc_x2_divide_32fc_u_H
+
+#include <inttypes.h>
+#include <volk/volk_complex.h>
+#include <float.h>
+
+
+
+#ifdef LV_HAVE_SSE3
+#include <pmmintrin.h>
+#include <volk/volk_sse3_intrinsics.h>
+
+static inline void
+volk_32fc_x2_divide_32fc_u_sse3(lv_32fc_t* cVector, const lv_32fc_t* numeratorVector,
+                                            const lv_32fc_t* denumeratorVector, unsigned int num_points)
+{
+    /*
+     * we'll do the "classical"
+     *  a      a b*
+     * --- = -------
+     *  b     |b|^2
+     * */
+  unsigned int number = 0;
+  const unsigned int quarterPoints = num_points / 4;
+
+  __m128 num01, num23, den01, den23, norm, result;
+  lv_32fc_t* c = cVector;
+  const lv_32fc_t* a = numeratorVector;
+  const lv_32fc_t* b = denumeratorVector;
+
+  for(; number < quarterPoints; number++){
+    num01 = _mm_loadu_ps((float*) a);    // first pair
+    den01 = _mm_loadu_ps((float*) b);    // first pair
+    num01 = _mm_complexconjugatemul_ps(num01, den01);   // a conj(b)
+    a += 2;
+    b += 2;
+
+    num23 = _mm_loadu_ps((float*) a);    // second pair
+    den23 = _mm_loadu_ps((float*) b);    // second pair
+    num23 = _mm_complexconjugatemul_ps(num23, den23);   // a conj(b)
+    a += 2;
+    b += 2;
+
+    norm = _mm_magnitudesquared_ps_sse3(den01, den23);
+
+    den01 = _mm_shuffle_ps(norm, norm, _MM_SHUFFLE(1,1,0,0));
+    den23 = _mm_shuffle_ps(norm, norm, _MM_SHUFFLE(3,3,2,2));
+
+    result = _mm_div_ps(num01, den01);
+    _mm_storeu_ps((float*) c, result); // Store the results back into the C container
+    c += 2;
+    result = _mm_div_ps(num23, den23);
+    _mm_storeu_ps((float*) c, result); // Store the results back into the C container
+    c += 2;
+  }
+
+  number *= 4;
+  for(;number < num_points; number++){
+    *c = (*a) / (*b);
+    a++; b++; c++;
+  }
+}
+#endif /* LV_HAVE_SSE */
+
+
+#ifdef LV_HAVE_GENERIC
+
+static inline void
+volk_32fc_x2_divide_32fc_generic(lv_32fc_t* cVector, const lv_32fc_t* aVector,
+                                             const lv_32fc_t* bVector, unsigned int num_points)
+{
+  lv_32fc_t* cPtr = cVector;
+  const lv_32fc_t* aPtr = aVector;
+  const lv_32fc_t* bPtr=  bVector;
+  unsigned int number = 0;
+
+  for(number = 0; number < num_points; number++){
+    *cPtr++ = (*aPtr++) / (*bPtr++);
+  }
+}
+#endif /* LV_HAVE_GENERIC */
+
+
+
+#endif /* INCLUDED_volk_32fc_x2_divide_32fc_u_H */
+#ifndef INCLUDED_volk_32fc_x2_divide_32fc_a_H
+#define INCLUDED_volk_32fc_x2_divide_32fc_a_H
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <volk/volk_complex.h>
+#include <float.h>
+
+
+#ifdef LV_HAVE_SSE3
+#include <pmmintrin.h>
+#include <volk/volk_sse3_intrinsics.h>
+
+static inline void
+volk_32fc_x2_divide_32fc_a_sse3(lv_32fc_t* cVector, const lv_32fc_t* numeratorVector,
+                                            const lv_32fc_t* denumeratorVector, unsigned int num_points)
+{
+    /*
+     * we'll do the "classical"
+     *  a      a b*
+     * --- = -------
+     *  b     |b|^2
+     * */
+  unsigned int number = 0;
+  const unsigned int quarterPoints = num_points / 4;
+
+  __m128 num01, num23, den01, den23, norm, result;
+  lv_32fc_t* c = cVector;
+  const lv_32fc_t* a = numeratorVector;
+  const lv_32fc_t* b = denumeratorVector;
+
+  for(; number < quarterPoints; number++){
+    num01 = _mm_load_ps((float*) a);    // first pair
+    den01 = _mm_load_ps((float*) b);    // first pair
+    num01 = _mm_complexconjugatemul_ps(num01, den01);   // a conj(b)
+    a += 2;
+    b += 2;
+
+    num23 = _mm_load_ps((float*) a);    // second pair
+    den23 = _mm_load_ps((float*) b);    // second pair
+    num23 = _mm_complexconjugatemul_ps(num23, den23);   // a conj(b)
+    a += 2;
+    b += 2;
+
+    norm = _mm_magnitudesquared_ps_sse3(den01, den23);
+
+    den01 = _mm_shuffle_ps(norm, norm, _MM_SHUFFLE(1,1,0,0));
+    den23 = _mm_shuffle_ps(norm, norm, _MM_SHUFFLE(3,3,2,2));
+
+    result = _mm_div_ps(num01, den01);
+    _mm_store_ps((float*) c, result); // Store the results back into the C container
+    c += 2;
+    result = _mm_div_ps(num23, den23);
+    _mm_store_ps((float*) c, result); // Store the results back into the C container
+    c += 2;
+  }
+
+  number *= 4;
+  for(;number < num_points; number++){
+    *c = (*a) / (*b);
+    a++; b++; c++;
+  }
+}
+#endif /* LV_HAVE_SSE */
+
+#ifdef LV_HAVE_GENERIC
+
+static inline void
+volk_32fc_x2_divide_32fc_a_generic(lv_32fc_t* cVector, const lv_32fc_t* aVector,
+                                               const lv_32fc_t* bVector, unsigned int num_points)
+{
+  lv_32fc_t* cPtr = cVector;
+  const lv_32fc_t* aPtr = aVector;
+  const lv_32fc_t* bPtr=  bVector;
+  unsigned int number = 0;
+
+  for(number = 0; number < num_points; number++){
+    *cPtr++ = (*aPtr++)  / (*bPtr++);
+  }
+}
+#endif /* LV_HAVE_GENERIC */
+
+
+#endif /* INCLUDED_volk_32fc_x2_divide_32fc_a_H */
