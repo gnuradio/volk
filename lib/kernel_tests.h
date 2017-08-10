b990779b08107e6c5fcc0a7001f5929662b4dad7
Jessica Iwamoto <jessica.iwamoto@aero.org>
2017-08-10 10:34:45 -0700

    Added AVX intrinsics to normalize, sqrt, max kernels

--- lib/kernel_tests.h
+++ lib/kernel_tests.h
@@ -30,6 +30,10 @@ std::vector<volk_test_case_t> init_test_list(volk_test_params_t test_params)
             test_params.vlen(), test_params.iter(), test_params.benchmark_mode(), test_params.kernel_regex());
 
     std::vector<volk_test_case_t> test_cases = boost::assign::list_of
+        (VOLK_INIT_TEST(volk_8i_convert_16i,                            test_params))
+        (VOLK_INIT_TEST(volk_32f_sqrt_32f,                              test_params_inacc))
+        (VOLK_INIT_TEST(volk_32f_s32f_normalize,                        test_params))
+        (VOLK_INIT_TEST(volk_32f_x2_max_32f,                            test_params))
         (VOLK_INIT_PUPP(volk_64u_popcntpuppet_64u, volk_64u_popcnt,     test_params))
 
         (VOLK_INIT_PUPP(volk_16u_byteswappuppet_16u, volk_16u_byteswap, test_params))
@@ -99,12 +103,12 @@ std::vector<volk_test_case_t> init_test_list(volk_test_params_t test_params)
         (VOLK_INIT_TEST(volk_32f_x2_dot_prod_32f,                       test_params_inacc))
         (VOLK_INIT_TEST(volk_32f_x2_s32f_interleave_16ic, volk_test_params_t(1, test_params.scalar(), test_params.vlen(), test_params.iter(), test_params.benchmark_mode(), test_params.kernel_regex())))
         (VOLK_INIT_TEST(volk_32f_x2_interleave_32fc,                    test_params))
-        (VOLK_INIT_TEST(volk_32f_x2_max_32f,                            test_params))
+
         (VOLK_INIT_TEST(volk_32f_x2_min_32f,                            test_params))
         (VOLK_INIT_TEST(volk_32f_x2_multiply_32f,                       test_params))
-        (VOLK_INIT_TEST(volk_32f_s32f_normalize,                        test_params))
+
         (VOLK_INIT_TEST(volk_32f_s32f_power_32f,                        test_params))
-        (VOLK_INIT_TEST(volk_32f_sqrt_32f,                              test_params_inacc))
+
         (VOLK_INIT_TEST(volk_32f_s32f_stddev_32f,                       test_params_inacc))
         (VOLK_INIT_TEST(volk_32f_stddev_and_mean_32f_x2,                test_params_inacc))
         (VOLK_INIT_TEST(volk_32f_x2_subtract_32f,                       test_params))
@@ -123,7 +127,7 @@ std::vector<volk_test_case_t> init_test_list(volk_test_params_t test_params)
         (VOLK_INIT_TEST(volk_8ic_deinterleave_real_8i,                  test_params))
         (VOLK_INIT_TEST(volk_8ic_x2_multiply_conjugate_16ic,            test_params))
         (VOLK_INIT_TEST(volk_8ic_x2_s32f_multiply_conjugate_32fc,       test_params))
-        (VOLK_INIT_TEST(volk_8i_convert_16i,                            test_params))
+
         (VOLK_INIT_TEST(volk_8i_s32f_convert_32f,                       test_params))
         (VOLK_INIT_TEST(volk_32fc_s32fc_multiply_32fc,                  test_params))
         (VOLK_INIT_TEST(volk_32f_s32f_multiply_32f,                     test_params))
