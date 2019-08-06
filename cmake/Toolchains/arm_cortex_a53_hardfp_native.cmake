2ce1657a6cefe907d118c776eebacc03d9c749be
Albin Stigo <albin.stigo@gmail.com>
2019-08-06 19:28:47 +0200

    Adds toolchain file for Raspberry Pi 3 and updates Raspberry 4 toolchain file

--- cmake/Toolchains/arm_cortex_a53_hardfp_native.cmake
+++ cmake/Toolchains/arm_cortex_a53_hardfp_native.cmake
@@ -4,6 +4,6 @@
 ########################################################################
 set(CMAKE_CXX_COMPILER g++)
 set(CMAKE_C_COMPILER  gcc)
-set(CMAKE_CXX_FLAGS "-march=armv8-a -mtune=cortex-a72 -mfpu=neon-fp-armv8 -mfloat-abi=hard" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-march=armv8-a -mtune=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS ${CMAKE_CXX_FLAGS} CACHE STRING "" FORCE) #same flags for C sources
 set(CMAKE_ASM_FLAGS "${CMAKE_CXX_FLAGS} -mthumb -g" CACHE STRING "" FORCE) #same flags for asm sources
