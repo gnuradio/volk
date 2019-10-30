b44f4492328be5826ea7226bde2b8e31bfa520a2
Carles Fernandez <carles.fernandez@gmail.com>
2019-10-30 15:36:53 +0100

    cmake: remove references to CMake < 3.8, add GPL header

--- cmake/Modules/FindFILESYSTEM.cmake
+++ cmake/Modules/FindFILESYSTEM.cmake
@@ -1,3 +1,26 @@
+# Copyright 2019 Free Software Foundation, Inc.
+#
+# This file is part of Volk
+#
+# Volk is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# Volk is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Volk; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Boston, MA
+# 02110-1301, USA.
+
+# Original code from https://github.com/vector-of-bool/CMakeCM and modified
+# by C. Fernandez. The original code is distributed under the OSI-approved
+# BSD 3-Clause License.
+
 #[=======================================================================[.rst:
 
 FindFILESYSTEM
