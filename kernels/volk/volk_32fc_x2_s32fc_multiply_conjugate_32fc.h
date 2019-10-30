ee7e018d92835ab20b4b8c6db900eab6e7db1d1a
Takehiro Sekine <takehiro.sekine@ps23.jp>
2019-10-30 21:59:42 +0900

    use existing intrinsics

--- kernels/volk/volk_32fc_x2_s32fc_multiply_conjugate_32fc.h
+++ kernels/volk/volk_32fc_x2_s32fc_multiply_conjugate_32fc.h
@@ -38,21 +38,51 @@
  * \b Inputs
  * \li aVector: The input vector to be added.
  * \li bVector: The input vector to be conjugate and multiplied.
- * \li scalar The complex scalar to multiply against conjugated bVector.
- * \li num_points: The number of complex values in aVector.
+ * \li scalar: The complex scalar to multiply against conjugated bVector.
+ * \li num_points: The number of complex values in aVector and bVector to be conjugate, multiplied and stored into cVector.
  *
  * \b Outputs
- * \li cVector: The vector where the results will be updated.
+ * \li cVector: The vector where the results will be stored.
  *
  * \b Example
+ * Calculate coefficients.
  *
  * \code
+ * int n_filter = 2 * N + 1;
+ * unsigned int alignment = volk_get_alignment();
  *
+ * // state is a queue of input IQ data.
+ * lv_32fc_t* state = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t) * n_filter, alignment);
+ * // weight and next one.
+ * lv_32fc_t* weight = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t) * n_filter, alignment);
+ * lv_32fc_t* next = (lv_32fc_t*)volk_malloc(sizeof(lv_32fc_t) * n_filter, alignment);
+ * ...
+ * // push back input IQ data into state.
+ * foo_push_back_queue(state, input);
+ *
+ * // get filtered output.
+ * lv_32fc_t output = lv_cmake(0.f,0.f);
+ * for (int i = 0; i < n_filter; i++) {
+ *   output += state[i] * weight[i];
+ * }
+ *
+ * // update weight using output.
+ * float real = lv_creal(output) * (1.0 - std::norm(output)) * MU;
+ * lv_32fc_t factor = lv_cmake(real, 0.f);
+ * volk_32fc_x2_s32fc_multiply_conjugate(next, weight, state, factor, n_filter);
+ * lv_32fc_t *tmp = next;
+ * next = weight;
+ * weight = tmp;
+ * weight[N + 1] = lv_cmake(lv_creal(weight[N + 1]), 0.f);
+ * ...
+ * lv_free(state);
+ * lv_free(weight);
+ * lv_free(next);
  * \endcode
  */
 
-#ifndef INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_H
-#define INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_H
+#ifndef INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_H
+#define INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_H
 
 #include <inttypes.h>
 #include <stdio.h>
@@ -60,44 +90,61 @@
 #include <float.h>
 
 
+#ifdef LV_HAVE_GENERIC
+
+static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_generic(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points){
+    const lv_32fc_t* aPtr = aVector;
+    const lv_32fc_t* bPtr = bVector;
+    lv_32fc_t* cPtr = cVector;
+    unsigned int number = num_points;
+
+    // unwrap loop
+    while (number >= 8) {
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+        number -= 8;
+    }
+
+    // clean up any remaining
+    while (number-- > 0) {
+        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
+    }
+}
+#endif /* LV_HAVE_GENERIC */
+
+
 #ifdef LV_HAVE_AVX
 #include <immintrin.h>
+#include <volk/volk_avx_intrinsics.h>
 
-static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_avx(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points) {
+static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_u_avx(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points) {
     unsigned int number = 0;
     unsigned int i = 0;
     const unsigned int quarterPoints = num_points / 4;
     unsigned int isodd = num_points & 3;
 
-    __m256 x, sl, sh, y, z, tmp1, tmp2;
+    __m256 x, y, s, z;
+    lv_32fc_t v_scalar[4] = {scalar, scalar, scalar, scalar};
 
     const lv_32fc_t* a = aVector;
     const lv_32fc_t* b = bVector;
     lv_32fc_t* c = cVector;
 
-    __m256 conjugator = _mm256_setr_ps(0, -0.f, 0, -0.f, 0, -0.f, 0, -0.f);
-
     // Set up constant scalar vector
-    sl = _mm256_set1_ps(lv_creal(scalar));
-    sh = _mm256_set1_ps(lv_cimag(scalar));
+    s = _mm256_loadu_ps((float*)v_scalar);
 
     for(;number < quarterPoints; number++) {
-        x = _mm256_load_ps((float*)b);
-        y = _mm256_load_ps((float*)a);
-
-        x = _mm256_xor_ps(x, conjugator);
-
-        tmp1 = _mm256_mul_ps(x,sl);
-
-        x = _mm256_shuffle_ps(x,x,0xB1);
-
-        tmp2 = _mm256_mul_ps(x,sh);
-
-        z = _mm256_addsub_ps(tmp1,tmp2);
-
+        x = _mm256_loadu_ps((float*)b);
+        y = _mm256_loadu_ps((float*)a);
+        z = _mm256_complexconjugatemul_ps(s, x);
         z = _mm256_add_ps(y, z);
-
-        _mm256_store_ps((float*)c,z);
+        _mm256_storeu_ps((float*)c,z);
 
         a += 4;
         b += 4;
@@ -107,47 +154,34 @@ static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_avx(lv_32fc_t* c
     for(i = num_points-isodd; i < num_points; i++) {
         *c++ = (*a++) + lv_conj(*b++) * scalar;
     }
-
 }
 #endif /* LV_HAVE_AVX */
 
 
 #ifdef LV_HAVE_SSE3
 #include <pmmintrin.h>
+#include <volk/volk_sse3_intrinsics.h>
 
-static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_sse3(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points) {
+static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_u_sse3(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points) {
     unsigned int number = 0;
     const unsigned int halfPoints = num_points / 2;
 
-    __m128 x, sl, sh, y, z, tmp1, tmp2;
+    __m128 x, y, s, z;
+    lv_32fc_t v_scalar[2] = {scalar, scalar};
 
     const lv_32fc_t* a = aVector;
     const lv_32fc_t* b = bVector;
     lv_32fc_t* c = cVector;
 
-    __m128 conjugator = _mm_setr_ps(0, -0.f, 0, -0.f);
-
     // Set up constant scalar vector
-    sl = _mm_set_ps1(lv_creal(scalar));
-    sh = _mm_set_ps1(lv_cimag(scalar));
+    s = _mm_loadu_ps((float*)v_scalar);
 
     for(;number < halfPoints; number++){
-        x = _mm_load_ps((float*)b);
-        y = _mm_load_ps((float*)a);
-
-        x = _mm_xor_ps(x, conjugator);
-
-        tmp1 = _mm_mul_ps(x,sl);
-
-        x = _mm_shuffle_ps(x,x,0xB1);
-
-        tmp2 = _mm_mul_ps(x,sh);
-
-        z = _mm_addsub_ps(tmp1,tmp2);
-
+        x = _mm_loadu_ps((float*)b);
+        y = _mm_loadu_ps((float*)a);
+        z = _mm_complexconjugatemul_ps(s, x);
         z = _mm_add_ps(y, z);
-
-        _mm_store_ps((float*)c,z);
+        _mm_storeu_ps((float*)c,z);
 
         a += 2;
         b += 2;
@@ -160,16 +194,81 @@ static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_sse3(lv_32fc_t* 
 }
 #endif /* LV_HAVE_SSE */
 
-#endif /* INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_H */
+
+#ifdef LV_HAVE_AVX
+#include <immintrin.h>
+#include <volk/volk_avx_intrinsics.h>
+
+static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_avx(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points) {
+    unsigned int number = 0;
+    unsigned int i = 0;
+    const unsigned int quarterPoints = num_points / 4;
+    unsigned int isodd = num_points & 3;
+
+    __m256 x, y, s, z;
+    lv_32fc_t v_scalar[4] = {scalar, scalar, scalar, scalar};
+
+    const lv_32fc_t* a = aVector;
+    const lv_32fc_t* b = bVector;
+    lv_32fc_t* c = cVector;
+
+    // Set up constant scalar vector
+    s = _mm256_load_ps((float*)v_scalar);
+
+    for(;number < quarterPoints; number++) {
+        x = _mm256_load_ps((float*)b);
+        y = _mm256_load_ps((float*)a);
+        z = _mm256_complexconjugatemul_ps(s, x);
+        z = _mm256_add_ps(y, z);
+        _mm256_store_ps((float*)c,z);
+
+        a += 4;
+        b += 4;
+        c += 4;
+    }
+
+    for(i = num_points-isodd; i < num_points; i++) {
+        *c++ = (*a++) + lv_conj(*b++) * scalar;
+    }
+}
+#endif /* LV_HAVE_AVX */
 
 
-#ifndef INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_u_H
-#define INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_u_H
+#ifdef LV_HAVE_SSE3
+#include <pmmintrin.h>
+#include <volk/volk_sse3_intrinsics.h>
 
-#include <inttypes.h>
-#include <stdio.h>
-#include <volk/volk_complex.h>
-#include <float.h>
+static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_a_sse3(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points) {
+    unsigned int number = 0;
+    const unsigned int halfPoints = num_points / 2;
+
+    __m128 x, y, s, z;
+    lv_32fc_t v_scalar[2] = {scalar, scalar};
+
+    const lv_32fc_t* a = aVector;
+    const lv_32fc_t* b = bVector;
+    lv_32fc_t* c = cVector;
+
+    // Set up constant scalar vector
+    s = _mm_load_ps((float*)v_scalar);
+
+    for(;number < halfPoints; number++){
+        x = _mm_load_ps((float*)b);
+        y = _mm_load_ps((float*)a);
+        z = _mm_complexconjugatemul_ps(s, x);
+        z = _mm_add_ps(y, z);
+        _mm_store_ps((float*)c,z);
+
+        a += 2;
+        b += 2;
+        c += 2;
+    }
+
+    if((num_points % 2) != 0) {
+        *c = *a + lv_conj(*b) * scalar;
+    }
+}
+#endif /* LV_HAVE_SSE */
 
 
 #ifdef LV_HAVE_NEON
@@ -217,31 +316,4 @@ static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_neon(lv_32fc_t* cV
 }
 #endif /* LV_HAVE_NEON */
 
-#ifdef LV_HAVE_GENERIC
-
-static inline void volk_32fc_x2_s32fc_multiply_conjugate_32fc_generic(lv_32fc_t* cVector, const lv_32fc_t* aVector, const lv_32fc_t* bVector, const lv_32fc_t scalar, unsigned int num_points){
-    const lv_32fc_t* aPtr = aVector;
-    const lv_32fc_t* bPtr = bVector;
-    lv_32fc_t* cPtr = cVector;
-    unsigned int number = num_points;
-
-    // unwrap loop
-    while (number >= 8){
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-        number -= 8;
-    }
-
-    // clean up any remaining
-    while (number-- > 0)
-        *cPtr++ = (*aPtr++) + lv_conj(*bPtr++) * scalar;
-}
-#endif /* LV_HAVE_GENERIC */
-
-#endif /* INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_u_H */
+#endif /* INCLUDED_volk_32fc_x2_s32fc_multiply_conjugate_32fc_H */
