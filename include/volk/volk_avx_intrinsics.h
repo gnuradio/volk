2bc699d1ba7bdd17cc3481f940d2535352579257
Johannes Demel <demel@uni-bremen.de>
2020-02-08 19:22:08 +0100

    rotator: Refactor AVX kernels
    Fix #331
    
    This is ongoing work to track down the root cause of #329.
    
    1. Make sure all input scalars are normalized -> no improvement
    2. Trigger renormalization after same num_points -> no improvement
    
    Lower tolerance values move the first point that breaks this bound
    further to the front. It is not obvious why this would happen so far.
    
    Since SSE kernels tend to break later, approx. factor 2, maybe the
    generic kernels accumulates errors faster. That's just a wild guess
    though.

--- include/volk/volk_avx_intrinsics.h
+++ include/volk/volk_avx_intrinsics.h
@@ -54,6 +54,17 @@ _mm256_complexconjugatemul_ps(__m256 x, __m256 y){
 }
 
 static inline __m256
+_mm256_normalize_ps(__m256 val)
+{
+  __m256 tmp1, tmp2;
+  tmp1 = _mm256_mul_ps(val, val);
+  tmp2 = _mm256_hadd_ps(tmp1, tmp1);
+  tmp1 = _mm256_shuffle_ps(tmp2, tmp2, 0xD8);
+  tmp2 = _mm256_sqrt_ps(tmp1);
+  return _mm256_div_ps(val, tmp2);
+}
+
+static inline __m256
 _mm256_magnitudesquared_ps(__m256 cplxValue1, __m256 cplxValue2){
   __m256 complex1, complex2;
   cplxValue1 = _mm256_mul_ps(cplxValue1, cplxValue1); // Square the values
