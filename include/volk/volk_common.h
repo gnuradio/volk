d1a4cc1f775b73c8a14ec2a27513f1d1cc977513
Johannes Demel <demel@uni-bremen.de>
2020-03-17 21:53:08 +0100

    clang-format: Update PR with GitHub Action

--- include/volk/volk_common.h
+++ include/volk/volk_common.h
@@ -69,7 +69,7 @@
 ////////////////////////////////////////////////////////////////////////
 #if defined(_MSC_VER)
 #pragma warning(disable : 4244) //'conversion' conversion from 'type1' to 'type2',
-                                //possible loss of data
+                                // possible loss of data
 #pragma warning(disable : 4305) //'identifier' : truncation from 'type1' to 'type2'
 #endif
 
